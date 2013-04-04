dc3f97a40df18acad5c4b2b431ffa7d0e2f8c096
Hippie Hacker <chris@hippiehacker.org>
2013-04-04 16:14:27 -0700

    include attributes

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['apache2']['windows']['source']="http://www.apachelounge.com/download/win32/binaries/httpd-2.2.24-win32-ssl_0.9.8-VC9.zip"
+default['apache2']['windows']['checksum']=nil
+default['apache2']['windows']['path']='c:\Apache24'
+
