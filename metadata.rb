4e128493f6e0fd33da19072d0a9574c13dd3a894
Yvo van Doorn <yvo@opscode.com>
2013-04-04 16:26:55 -0700

    change suggests to depends

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,9 @@ description      "Installs/Configures apache_win"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+%w{ windows }.each do |os|
+  supports os
+end
 
-suggests          "windows"
-suggests          "vcruntime"
+depends          "windows"
+depends          "vcruntime"
