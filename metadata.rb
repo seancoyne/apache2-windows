7cb8a89fd59027d86ecc18b5280f0c0f6d007a5b
Hippie Hacker <chris@hippiehacker.org>
2013-04-04 11:32:21 -0700

    made opscode the maintainer

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-maintainer       "YOUR_COMPANY_NAME"
+maintainer       "Opscode, Inc"
 maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "Installs/Configures apache_win"
